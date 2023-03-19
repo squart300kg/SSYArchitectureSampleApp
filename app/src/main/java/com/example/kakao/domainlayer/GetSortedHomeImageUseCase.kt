d91539429b62136f5e658d5f340ca2ff273f52d7
n22301003-sysong <sysong@pentasecurity.com>
2023-03-19 23:03:22 +0900

    사용할 중간 연산자 리서치

--- app/src/main/java/com/example/kakao/domainlayer/GetSortedHomeImageUseCase.kt
+++ app/src/main/java/com/example/kakao/domainlayer/GetSortedHomeImageUseCase.kt
@@ -1,11 +1,18 @@
 package com.example.kakao.domainlayer
 
 import com.example.kakao.datalayer.repository.ImageRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
 import javax.inject.Inject
 
 class GetSortedHomeImageUseCase @Inject constructor(
-    imageRepository: ImageRepository
+    private val imageRepository: ImageRepository
 ) {
 
-    operator fun invoke() {}
+    // TODO: localImages와 remoteImages의 병렬연산을 통해 하나의 리스트로 만들어주기 위한
+    //  flatMapConcat or flatMapMerge or flatMapLatest의 사용 고려?
+    //  https://kotlinworld.com/263 참고
+    operator fun invoke(): Flow<String> {
+        return flowOf("s")
+    }
 }
--- app/src/main/java/com/example/kakao/uilayer/ui/search/SearchResultViewModel.kt
+++ app/src/main/java/com/example/kakao/uilayer/ui/search/SearchResultViewModel.kt
@@ -3,19 +3,35 @@ package com.example.kakao.uilayer.ui.search
 import android.util.Log
 import android.view.View
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.kakao.domainlayer.GetSortedHomeImageUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
 
 @HiltViewModel
 class SearchResultViewModel @Inject constructor(
-    getSortedHomeImageUseCase: GetSortedHomeImageUseCase
+    private val getSortedHomeImageUseCase: GetSortedHomeImageUseCase
 ) : ViewModel() {
 
+    // TODO: 연속 검색을 방지하기 위한 debounce?
+    // TODO: 동일 검색 결과를 방지하기 위한 disticntUntilChanged?
     fun search(keyWord: String) {
         Log.i("key", keyWord)
+        viewModelScope.launch {
+            getSortedHomeImageUseCase()
+                .flowOn(Dispatchers.IO)
+                .catch {  }
+                .collect {
+
+                }
+
+
+        }
     }
 
 }
